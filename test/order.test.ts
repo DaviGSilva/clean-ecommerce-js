552fd74c78412b69cf2d9268f86c1011090792a7
davi.silva <davi.silva@westwing.com.br>
2023-04-02 23:40:23 -0300

    [TDD] Add +2 Tests to Order
    Add order test with discount coupon and another with invalid CPF.
    TODO: Create the classes to product and order.
    TODO: Update CPF validation logic.

--- test/order.test.ts
+++ test/order.test.ts
@@ -1,9 +1,26 @@
-test('should calculate total price from order with 3 products', () => {
+test('should calculate order price with 3 products', () => {
     const products = getProducts();
     const subtotal = calculateSubtotal(products);
     const order = { products, subtotal };
     expect(order.products.length).toBe(3);
-    expect(order.subtotal).toBe(1229.99);
+    expect(order.subtotal).toBeCloseTo(1229.99);
+});
+
+test('should calculate order price with discount coupon', () => {
+    const products = getProducts();
+    const discount = 50;
+    const subtotal = calculateSubtotal(products, discount);
+    const order = { products, subtotal, discount };
+    expect(order.products.length).toBe(3);
+    expect(order.subtotal).toBeCloseTo(614.99);
+});
+
+test('should throw an exception when invalid CPF is given', () => {
+    const cpf = '';
+    const products = getProducts();
+    const subtotal = calculateSubtotal(products);
+    const order = { products, subtotal, cpf };
+    expect(() => validateCpf(order.cpf)).toThrow(new Error('Invalid CPF'));
 });
 
 function getProducts() {
@@ -26,10 +43,22 @@ function getProducts() {
     ];
 }
 
-function calculateSubtotal(products) {
+function calculateSubtotal(products: Array<any>, discountPercent?: number) {
     const initialValue = 0;
-    return products.reduce(
+    const subtotal = products.reduce(
         (accumulator, product) => accumulator + product.price,
         initialValue
     );
+    if (!discountPercent) {
+        return subtotal;
+    }
+    const discount = subtotal * (discountPercent/100);
+    return subtotal - discount;
+}
+
+function validateCpf(cpf: string) {
+    if (!cpf || cpf.length < 0) {
+        throw new Error('Invalid CPF');
+    }
+    return true;
 }
